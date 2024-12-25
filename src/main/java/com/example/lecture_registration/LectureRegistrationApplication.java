f9518f3b4cd7fcb991b365902e6549e317657357
bje <baejungeun11@gmail.com>
2024-12-25 20:21:39 +0900

    project init

---
+++ src/main/java/com/example/lecture_registration/LectureRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecture_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureRegistrationApplication.class, args);
+    }
+
+}
