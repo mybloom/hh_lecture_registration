7bfe747109a0fc68096d498afcd3ffea3015ed52
bje <baejungeun11@gmail.com>
2024-12-26 22:00:40 +0900

    Lecture, LectureSchedule 도메인/테스트 작성

---
+++ src/main/java/com/example/lecture_registration/lecture/domain/Lecture.java
@@ -0,0 +1,32 @@
+package com.example.lecture_registration.lecture.domain;
+
+import com.example.lecture_registration.global.database.BaseTimeEntity;
+import com.example.lecture_registration.instructor.domain.Instructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Lecture extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    private int quota;
+
+}
